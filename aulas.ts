92eae1baca3f79eba05fb765b638266112e97c6e
Bruno Venturi <brunomitopvp@gmail.com>
2025-03-17 20:37:31 -0300

    Exercicio 2

--- aulas.ts
+++ aulas.ts
@@ -2,3 +2,11 @@
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
+
+
+
+//Exercício 2 - Função Tipada
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
